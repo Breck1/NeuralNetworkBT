b35e4dff6c63758b4ec5de7e0063059d2c19fbdf
BeneFager <Benefager@hotmail.com>
2022-03-18 11:39:14 +0100

    NeuralNetwork method finished

--- TestLua/NeuralNetwork.h
+++ TestLua/NeuralNetwork.h
@@ -5,15 +5,6 @@
 #include <mlpack/core.hpp>
 
 
-inline float Sigmoid(float x)
-{
-	return 1.0f / (1 + exp(-x));
-}
-
-inline float DSigmoid(float x)
-{
-	return (x * (1 - x));
-}
 
 class NeuralNetwork //??
 {
@@ -21,10 +12,39 @@ class NeuralNetwork //??
 public:
 
 
-	void GenerateOutputs(int layers, int weights)
+	std::vector<float> GenerateOutputs(int layers, std::vector<float> weights, std::vector<float> inputs)
 	{
+		std::vector<float> activations;
+		std::vector<float> nextActivations;
+		int weightIndex = 0;
 
+		activations = inputs;
+		nextActivations.resize(activations.size());
 
+		for(int i = 0; i < layers - 1; i++)
+		{
+			for(int j = 0; j < layers + 1; j++)
+			{
+				//set neuron activation to bias value
+				nextActivations[j] = weights[weightIndex];
+				weightIndex++;
+
+				for(int k = 0; k < layers; k++)
+				{
+					//add inputs * weight 
+					nextActivations[j] = nextActivations[j] + weights[weightIndex] * activations[k];
+					weightIndex++;
+				}
+
+				//sigmoid
+				nextActivations[j] = 1 / (1 + exp(-nextActivations[j]));
+			}
+			//go to next layer
+			activations = nextActivations;
+			nextActivations.clear();
+		}
+		return activations;
 	}
+
 };
 
