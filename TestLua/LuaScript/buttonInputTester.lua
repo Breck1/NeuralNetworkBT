de06b8d2c81ba3af649cfe8b363288689c7098ec
BeneFager <Benefager@hotmail.com>
2022-03-28 10:24:55 +0200

    TestInputlua

---
+++ TestLua/LuaScript/buttonInputTester.lua
@@ -0,0 +1,49 @@
+
+
+ButtonInput2 = {}
+
+
+function ButtonInput2.new(resultSize)
+
+    local self = {}
+
+
+    self.left = false
+    self.right = false
+    self.up = false
+    self.down = false
+
+    self.b = false
+    self.a = false
+    self.y = false
+    self.x = false
+
+    self.l = false
+    self.r = false
+
+    self.start = false
+    self.select = false
+
+
+    local results = {}
+    function SetInputCPP(index, boolean)
+
+        results[index] = boolean
+        if results[index] == true
+        then
+            print("[LUA: InputOutput SetInputCPP: buttonInput["..index.. "] is true\n")
+        else
+            print("[LUA: InputOutput temp: buttonInput["..index.. "] is false\n")
+        end
+        return results
+    end
+
+    function SetResults(resultsTable)
+        results = resultsTable
+        return results
+    end
+
+
+    return self
+
+end
