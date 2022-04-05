a33b6371ec53eaaf96382c2f554a0ec48b5bac5a
Breck1 <rundstrom.tony@gmail.com>
2022-04-05 15:52:26 +0200

    Mapped Highwaystage with platforms, pitfalls, and destroyable segments. Positions based on Megaman's positions

---
+++ TestLua/LuaScript/HighwayMapLayout.lua
@@ -0,0 +1,278 @@
+HighwayStage = 
+{
+segmentWidth = 48
+mapXStart = 0
+mapXFinsih = 960
+mapYStart = 367
+
+	firstSegment = 
+	{
+		XPositionStart = 8
+		XPositionFinish = 806
+		YPosition = 367
+	}
+	
+	firstHole = 
+	{
+		XPositionStart = 807
+		XPositionFinish = 824
+	}
+	
+	secondSegment = 
+	{
+		XPositionStart = 825
+		XPositionFinish = 1318
+		PlatformDestructionPositions = 
+		{
+			XFirstStart = 1031
+			XFirstFinish = 1112
+
+			XSecondStart = 1159
+			XSecondFinish = 1240
+		}
+
+		YPosition = 335
+	}
+	
+	secondHole = 
+	{
+		XPositionStart = 1319
+		XPositionFinish = 1336
+	}
+	
+	thirdSegment =
+	{
+		XPositionStart = 1337
+		XPositionFinish = 2342
+
+		PlatformDestructionPositions = 
+		{
+			XFirstStart = 1415
+			XFirstFinish = 1496
+
+			XSecondStart = 1543
+			XSecondFinish = 1624
+
+			XThirdStart = 1671
+			XThirdFinish = 1752
+
+			XFourthStart = 1799
+			XFourthFinish = 1880
+
+			XFifthStart = 1927
+			XFifthFinish = 2008
+		}
+
+		YPosition = 351
+	}
+	
+	thirdHole = 
+	{
+		XPositionStart = 2343
+		XPositionFinish = 2376
+	}
+	
+	fourthSegment =
+	{
+		XPositionStart = 2377
+		XPositionFinish = 2566
+
+		YPosition = 351
+	}
+	
+	firstMiniboss =
+	{
+		XPositionStart = 2567
+		XPositionFinish = 2776
+		
+		YPositionStart = 351
+		YPositionFinish = 575
+	}
+	
+	fifthSegment =
+	{
+		XPositionStart = 2777
+		XPositionFinish = 2902
+
+		YPosition = 351
+	}
+
+	sixthSegment =
+	{
+		XPositionStart = 2905
+		XPositionFinish = 2936
+		YPosition = 431
+	}
+	
+	seventhSegment =
+	{
+		XPositionStart = 2937
+		XPositionFinish = 3206
+		YPosition = 383
+	}
+	
+	secondMiniBoss = 
+	{
+		XPositionStart = 3207
+		XPositionFinish = 3416
+	
+		YPositionStart = 351
+		YPositionFinish = 575
+	}
+	
+	eightSegment =
+	{
+		XPositionStart = 3417
+		XPositionFinish = 3718
+
+		YPosition = 383
+	}
+	
+	fourthHole = 
+	{
+		XPositionStart = 3719
+		XPositionFinish 3768
+	}
+	
+	nineSegment = 
+	{
+		XPositionStart = 3769
+		XPositionFinish = 3974
+
+		YPositionStart = 383
+		YPositionFinish = 351
+
+		InclineEndPositionX = 3903
+	}
+	
+	fifthHole =
+	{
+		XPositionStart = 3975
+		XPositionFinish = 4056
+	}
+	
+	tenthSegment = 
+	{
+		XPositionStart = 4057
+		XPositionFinish = 4230
+
+		YPosition = 351
+	}
+	
+	sixthHole = 
+	{
+		XPositionStart = 4231
+		XPositionFinish = 4312
+	}
+	
+	eleventhSegment = 
+	{
+		XPositionStart = 4313
+		XPositionFinish = 4358
+
+		YPosition = 319
+	}
+	
+	seventhHole =
+	{
+		XPositionStart = 4359
+		XPositionFinish = 4440
+	}
+	
+	twelvethSegment = 
+	{
+		XPositionStart = 4441
+		XPositionFinish = 4486
+
+		YPosition = 319
+	}
+	
+	eighthHole = 
+	{
+		XPositionStart = 4487
+		XPositionFinish = 4568	
+	}
+	
+	thirteenthSegment = 
+	{
+		XPositionStart = 4569
+		XPositionFinish = 4614
+
+		YPosition = 319
+	}
+	
+	ninthHole = 
+	{
+		XPositionStart = 4615
+		XPositionFinish = 4696
+	}
+	
+	fourteenthSegement = 
+	{
+		XPositionStart = 4697
+		XPositionFinish = 4742
+
+		YPosition = 319
+	}
+
+	tenthHole =
+	{
+		XPositionStart = 4743
+		XPositionFinish = 4824
+	}
+
+	fifteenthSegment = 
+	{
+		XPositionStart = 4825
+		XPositionFinish = 5303
+
+		YPositionStart = 351
+		YPositionFinish = 383
+
+		DeclineStartPositionX = 4867
+		DeclineFinishPositionX = 4991
+	}
+	
+	eleventhHole =
+	{
+		XPositionStart = 5304
+		XPositionFinish = 5336
+	}
+	
+	sixteenthSegment =
+	{
+		XPositionStart = 5337
+		XPositionFinish = 5638
+
+		YPosition = 351
+	}
+	
+	twelvethHole = 
+	{
+		XPositionStart = 5639
+		XPositionFinish = 5704
+	}
+	
+	seventeenthSegment =
+	{
+		XPositionStart = 5705
+		XPositionFinish = 6166
+
+		YPosition = 383
+	}
+	
+	thirteenthHole = 
+	{
+		XPositionStart = 6167
+		XPositionFinish = 6232
+	}
+
+	eighteenthSegment = 
+	{
+		XPositionStart = 6233
+		XPositionFinish = 7672
+
+		YPosition = 431
+	}
+}
+
